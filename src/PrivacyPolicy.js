0fbf9c1121d14edb82c8c2a907a2f9680537199e
Hidai Bar-Mor <hidai25@gmail.com>
2023-10-03 00:36:58 +0300

    added text

---
+++ src/PrivacyPolicy.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import './PrivacyPolicy.css';
+
+const PrivacyPolicy = () => (
+    <div className="privacy-policy">
+        <h1>Privacy Policy of WristRoad</h1>
+        
+        <p>In order to receive information about your Personal Data, the purposes and the parties the Data is shared with, contact the Owner.</p>
+
+        <h2>Owner and Data Controller</h2>
+        <p>Hidai Bar-Mor , 4 Natan Elbaz , Herzliya, Israel</p>
+        <p>Owner contact email: <a href="mailto:hidai25@gmail.com">hidai25@gmail.com</a></p>
+        
+        <h2>Types of Data collected</h2>
+        <p>The owner does not provide a list of Personal Data types collected. Complete details on each type of Personal Data collected are provided in the dedicated sections of this privacy policy or by specific explanation texts displayed prior to the Data collection. Personal Data may be freely provided by the User, or, in case of Usage Data, collected automatically when using this Application.</p>
+        <p>Unless specified otherwise, all Data requested by this Application is mandatory and failure to provide this Data may make it impossible for this Application to provide its services. In cases where this Application specifically states that some Data is not mandatory, Users are free not to communicate this Data without consequences to the availability or the functioning of the Service. Users who are uncertain about which Personal Data is mandatory are welcome to contact the Owner. Any use of Cookies – or of other tracking tools — by this Application or by the owners of third-party services used by this Application serves the purpose of providing the Service required by the User, in addition to any other purposes described in the present document.</p>
+        <p>Users are responsible for any third-party Personal Data obtained, published or shared through this Application.</p>
+
+        <h2>Mode and place of processing the Data</h2>
+        <h3>Methods of processing</h3>
+        <p>The Owner takes appropriate security measures to prevent unauthorized access, disclosure, modification, or unauthorized destruction of the Data. The Data processing is carried out using computers and/or IT enabled tools, following organizational procedures and modes strictly related to the purposes indicated. In addition to the Owner, in some cases, the Data may be accessible to certain types of persons in charge, involved with the operation of this Application (administration, sales, marketing, legal, system administration) or external parties (such as third-party technical service providers, mail carriers, hosting providers, IT companies, communications agencies) appointed, if necessary, as Data Processors by the Owner. The updated list of these parties may be requested from the Owner at any time.</p>
+
+        <h3>Place</h3>
+        <p>The Data is processed at the Owner's operating offices and in any other places where the parties involved in the processing are located. Depending on the User's location, data transfers may involve transferring the User's Data to a country other than their own. To find out more about the place of processing of such transferred Data, Users can check the section containing details about the processing of Personal Data.</p>
+
+        <h3>Retention time</h3>
+        <p>Unless specified otherwise in this document, Personal Data shall be processed and stored for as long as required by the purpose they have been collected for and may be retained for longer due to applicable legal obligation or based on the Users’ consent.</p>
+
+        <h2>Further Information for Users</h2>
+        <h3>Legal basis of processing</h3>
+        <ul>
+            <li>Users have given their consent for one or more specific purposes.</li>
+            <li>Provision of Data is necessary for the performance of an agreement with the User and/or for any pre-contractual obligations thereof;</li>
+            <li>Processing is necessary for compliance with a legal obligation to which the Owner is subject;</li>
+            <li>Processing is related to a task that is carried out in the public interest or in the exercise of official authority vested in the Owner;</li>
+            <li>Processing is necessary for the purposes of the legitimate interests pursued by the Owner or by a third party.</li>
+        </ul>
+        <p>In any case, the Owner will gladly help to clarify the specific legal basis that applies to the processing, and in particular whether the provision of Personal Data is a statutory or contractual requirement, or a requirement necessary to enter into a contract.</p>
+
+        <h2>Further information about retention time</h2>
+<p>Unless specified otherwise in this document, Personal Data shall be processed and stored for as long as required by the purpose they have been collected for and may be retained for longer due to applicable legal obligation or based on the Users’ consent.</p>
+
+<p>Therefore:</p>
+<ul>
+    <li>Personal Data collected for purposes related to the performance of a contract between the Owner and the User shall be retained until such contract has been fully performed.</li>
+    <li>Personal Data collected for the purposes of the Owner’s legitimate interests shall be retained as long as needed to fulfill such purposes. Users may find specific information regarding the legitimate interests pursued by the Owner within the relevant sections of this document or by contacting the Owner.</li>
+    </ul>
+<p>The Owner may be allowed to retain Personal Data for a longer period whenever the User has given consent to such processing, as long as such consent is not withdrawn. Furthermore, the Owner may be obliged to retain Personal Data for a longer period whenever required to fulfil a legal obligation or upon order of an authority.</p>
+
+<p>Once the retention period expires, Personal Data shall be deleted. Therefore, the right of access, the right to erasure, the right to rectification and the right to data portability cannot be enforced after expiration of the retention period.</p>
+
+{/* Rights of Users based on the GDPR */}
+<h2>The rights of Users based on the General Data Protection Regulation (GDPR)</h2>
+<p>Users may exercise certain rights regarding their Data processed by the Owner.</p>
+<ul>
+<ul>
+    <li><strong>Withdraw their consent at any time.</strong> Users have the right to withdraw consent where they have previously given their consent to the processing of their Personal Data.</li>
+    <li><strong>Object to processing of their Data.</strong> Users have the right to object to the processing of their Data if the processing is carried out on a legal basis other than consent.</li>
+    <li><strong>Access their Data.</strong> Users have the right to learn if Data is being processed by the Owner, obtain disclosure regarding certain aspects of the processing and obtain a copy of the Data undergoing processing.</li>
+    <li><strong>Verify and seek rectification.</strong> Users have the right to verify the accuracy of their Data and ask for it to be updated or corrected.</li>
+    <li><strong>Restrict the processing of their Data.</strong> Users have the right to restrict the processing of their Data. In this case, the Owner will not process their Data for any purpose other than storing it.</li>
+    <li><strong>Have their Personal Data deleted or otherwise removed.</strong> Users have the right to obtain the erasure of their Data from the Owner.</li>
+    <li><strong>Receive their Data and have it transferred to another controller.</strong> Users have the right to receive their Data in a structured, commonly used and machine readable format and, if technically feasible, to have it transmitted to another controller without any hindrance.</li>
+    <li><strong>Lodge a complaint.</strong> Users have the right to bring a claim before their competent data protection authority.</li>
+</ul>
+
+</ul>
+<p>Users are also entitled to learn about the legal basis for Data transfers abroadincluding to any international organization governed by public international law or set up by two or more countries, such as the UN, and about the security measures taken by the Owner to safeguard their Data.</p>
+
+{/* Details about the right to object to processing */}
+<h2>Details about the right to object to processing</h2>
+<p>Where Personal Data is processed for a public interest, in the exercise of an official authority vested in the Owner or for the purposes of the legitimate interests pursued by the Owner, Users may object to such processing by providing a ground related to their particular situation to justify the objection.</p>
+<p>Users must know that, however, should their Personal Data be processed for direct marketing purposes, they can object to that processing at any time, free of charge and without providing any justification. Where the User objects to processing for direct marketing purposes, the Personal Data will no longer be processed for such purposes. To learn whether the Owner is processing Personal Data for direct marketing purposes, Users may refer to the relevant sections of this document.</p>
+
+{/* How to exercise these rights */}
+<h3>How to exercise these rights</h3>
+<p>Any requests to exercise User rights can be directed to the Owner through the contact details provided in this document. Such requests are free of charge and will be answered by the Owner as early as possible and always within one month, providing Users with the information required by law. Any rectification or erasure of Personal Data or restriction of processing will be communicated by the Owner to each recipient, if any, to whom the Personal Data has been disclosed unless this proves impossible or involves disproportionate effort. At the Users’ request, the Owner will inform them about those recipients.</p>
+
+{/* Further information for Users in Switzerland */}
+<h2>Further information for Users in Switzerland</h2>
+<p>This section applies to Users in Switzerland, and, for such Users, supersedes any other possibly divergent or conflicting information contained in the privacy policy.</p>
+<p>Further details regarding the categories of Data processed, the purposes of processing, the categories of recipients of the personal data, if any, the retention period and further information about Personal Data can be found in the section titled “Detailed information on the processing of Personal Data” within this document.</p>
+
+{/* Rights of Users according to the Swiss Federal Act on Data Protection */}
+<h3>The rights of Users according to the Swiss Federal Act on Data Protection</h3>
+<p>Users may exercise certain rights regarding their Data within the limits of law, including the following:</p>
+<ul>
+    <li>right of access to Personal Data;</li>
+    <li>right to object to the processing of their Personal Data (which also allows Users to demand that processing of Personal Data be restricted, Personal Data be deleted or destroyed, specific disclosures of Personal Data to third parties be prohibited);</li>
+    <li>right to receive their Personal Data and have it transferred to another controller (data portability);</li>
+    <li>right to ask for incorrect Personal Data to be corrected.</li>
+</ul>
+<h3><strong>How to exercise these rights</strong></h3>
+<p>Any requests to exercise User rights can be directed to the Owner through the contact details provided in this document. Such requests are free of charge and will be answered by the Owner as early as possible, providing Users with the information required by law.
+
+</p>
+    </div>
+);
+
+export default PrivacyPolicy;
